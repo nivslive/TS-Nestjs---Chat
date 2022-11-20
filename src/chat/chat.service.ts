0a45ff786a44f7ea827d7c7de91c4eee5ff9d3de
nivslive <nivsoficial@gmail.com>
2022-11-20 00:19:29 -0300

    mocked static data

---
+++ src/chat/chat.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { UpdateChatDto } from './dto/update-chat.dto';
+
+@Injectable()
+export class ChatService {
+  create(createChatDto: CreateChatDto) {
+    return 'This action adds a new chat';
+  }
+
+  findAll() {
+    const data: Object[] = []
+    const rooms: Object[] = [
+      'maquintosh', 'roomdasarabias', 'matoresh'
+    ]
+    rooms.map((v, k) => {
+      const json = {
+        'id': k,
+        'room': v
+      }
+      data.push(json)
+    })
+
+    return data;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} chat`;
+  }
+
+  update(id: number, updateChatDto: UpdateChatDto) {
+    return `This action updates a #${id} chat`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} chat`;
+  }
+}
