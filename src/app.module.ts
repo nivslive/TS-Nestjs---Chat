e5d56257fb9caf1f0d761683216a2ce231176156
nivslive <nivsoficial@gmail.com>
2022-11-19 23:58:07 -0300

    att on port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatModule } from './chat/chat.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Chat } from './chat/entities/chat.entity';
+
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'sqlite',
-        database: 'database.db',
-        entities: [Chat],
-        synchronize: true, //development only
-      }),
-    }),
-    ChatModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/chat/dto/create-chat.dto.ts
+++
@@ -1,3 +0,0 @@
-export class CreateChatDto {
-  room: string;
-}
