468fb73dafb276f71f129353630afcfa1bc8cca6
nivslive <nivsoficial@gmail.com>
2022-11-26 15:12:26 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3333',
+      'http://localhost:8000',
+      'http://localhost:3001',
+    ],
+  });
   await app.listen(3333);
 }
 bootstrap();
